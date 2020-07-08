1645c5693a789d17f614c3db8c77b3e1ffd1d859
Alex_Kelly <afkelly14@Gmail.com>
2020-07-08 10:20:31 -0400

    added form buttons to HTML

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 /* a function so when the correct answer adds points*/
 /*points should increase as questions are asked*/
+
+/* SAMPLE FUNCTIONS
+
+function StartOver {
+
+}
+
+function correctAnswer{
+   
+}
+
+function lastAnswer {
+    
+}
