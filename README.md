664ced03e74a161cebb6b272f940ece1dcc380e4
Alex_Kelly <afkelly14@Gmail.com>
2020-07-09 18:39:59 -0400

    I was committing to the wrong project....I promise I was committing a lot :/

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # Project-4
 
-Trivia Game
-
 Title: Trivia Game
 
-Description: This project will utilize **html, css, javascript** to create a _self-scoring_ trivia game
+Description: This project will utilize **html, css, javascript** to create a _self-scoring_ trivia game about random music trivia
 
-    HTML: list of questions, each answer is a button
-    JAVASCRIPT: event listeners to the correct button with a certain number of points attached to it
-    CSS: colorful game page, if there is time
+    HTML: list of questions, set of questions is a form
+    JAVASCRIPT: correct answers add to the score, submit button will automatically show score, timeout function at onload to automatically submit the quiz if timer runs out
+    CSS: colorful game page
 
 
     option functions:
@@ -20,3 +18,8 @@ Example:
 Features:
 -self scoring
 -questions/bullets are worth different points based on difficulty of the question
+
+Issues:
+
+-added a form radio button, but all were clicking. Added a name="" to make only one in a set clickable.
+-tried to change the css of the radio buttons but couldn't figure it out
--- script.js
+++ script.js
@@ -1,16 +1,51 @@
-/* a function so when the correct answer adds points*/
-/*points should increase as questions are asked*/
+/*declare the variables*/
+let answers = document.querySelectorAll(".Answers");
 
-/* SAMPLE FUNCTIONS
+let button = document.querySelectorAll(".Incorrect");
 
-function StartOver {
+let correct = document.querySelectorAll(".Correct");
 
+let score = 0;
+
+let myQuestions = [];
+
+let gameOver = document.querySelector(".game-over");
+
+let Questions = document.querySelectorAll(".Questions");
+
+let submitButton = document.querySelector("button");
+
+console.log(answers.length);
+console.log(correct);
+
+/*loop to make the correct buttons clickable with some event*/
+
+for (i = 0; i < correct.length; i++) {
+  let correctAnswer = correct[i];
+
+  correctAnswer.addEventListener("click", itsCorrect);
+
+  function itsCorrect(e) {
+    score += 10;
+  }
 }
 
-function correctAnswer{
-   
+/*function to submit the game */
+
+let popUp = document.querySelector(".final-score");
+submitButton.addEventListener("click", submitGame);
+
+/*window with score pops up*/
+
+function submitGame() {
+  popUp.innerText = `Your Score Is ${score}`;
+  popUp.style.opacity = 1;
+  console.log(popUp.innerText);
 }
 
-function lastAnswer {
-    
-}
+/*on loadl function*/
+window.onload = alert("You have 2 minutes to complete this quiz!");
+
+/*timeout function*/
+
+window.setTimeout(submitGame, 100000);
